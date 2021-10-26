ea075afbefd021a3a8081e9f77856c78e8161121
Dmitriy <d.a.lukashenko@ukr.net>
2021-10-26 10:54:56 +0300

    3-module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,8 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  users.forEach(element => {
+    (age >= element.age)
+    ? (str += `${element.name}, ${element.balance}\n`)
+    : str});
+  return str.slice(0, -1);
 }
