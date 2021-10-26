e316bbb98f11986391270507a8a0a485aba4509d
Dmitriy <d.a.lukashenko@ukr.net>
2021-10-26 08:32:28 +0300

    module-3 task-2

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,31 +1,6 @@
 function namify(users) {
-// 1. 'for' using
-  // for (let i = 0; i < users.length; i++) {
-  //   users[i] = users[i].name;
-  // }
-  //   return users;
-
-// 2. forEach method using with theArr
-
-    // users.forEach(function(element, index, theArr){
-    //   theArr[index] = theArr[index].name;
-    // });
-    // return users;
-
-// 3. forEach method using with 'this'
-
-    // users.forEach(function(element, index) {
-    //     this[index] = this[index].name;
-    // }, users);
-    // return users;
-
-// 4. forEach method using with arrow function
-
-    // users.forEach((element, index, theArr) => theArr[index] = theArr[index].name);
-    // return users;
-
-// 5. 'map' method using
-
-  const modifyUsers = users.map(item => item.name);
-  return modifyUsers;
+  for (let i = 0; i < users.length; i++) {
+    users[i] = users[i].name;
+  }
+  return users;
 }
