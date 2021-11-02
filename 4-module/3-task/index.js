975af2aa80b1454978b6ae22cd578bb54d9b2a8f
Dmitriy <d.a.lukashenko@ukr.net>
2021-11-02 16:33:14 +0200

    4-module 1_2_3-tasks

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,61 @@
 function highlight(table) {
-  // ваш код...
+
+let thead = table.querySelector('thead');
+
+let headTd = thead.querySelectorAll('td');
+
+let tbody = table.querySelector('tbody');
+
+  for (let i = 0; i < headTd.length ; i++) {
+
+    let cell = headTd[i];
+      
+    if (cell.textContent === 'Age') {
+            
+      let trs = tbody.querySelectorAll('tr');
+        
+      for (let tr of trs) {
+               
+        let tds = tr.querySelectorAll('td');
+       
+        let tdAge = tds[i].textContent;
+        
+        if (tdAge < 18) {
+          tr.style.textDecoration = 'line-through';
+        }
+      }
+    } else if (cell.textContent === 'Gender') {
+      
+      let trs = tbody.querySelectorAll('tr');
+
+      for (let tr of trs) {
+        
+        let tds = tr.querySelectorAll('td');
+        let textTdGender = tds[i].innerHTML;
+       
+        if (textTdGender === 'm') {
+          tr.classList.add('male');
+        }
+        else if (textTdGender === 'f') {
+          tr.classList.add('female');
+        }
+      }
+    } else if (cell.textContent === 'Status') {
+      let trs = tbody.querySelectorAll('tr');
+      for (let tr of trs) {   
+        let tds = tr.querySelectorAll('td');
+        let tdStatus = tds[i].dataset.available;
+        if (tdStatus === 'true') {
+          tr.classList.add('available');
+        }
+        else if (tdStatus === 'false') {
+          tr.classList.add('unavailable');
+        }
+        else {
+          tr.hidden = true;
+        }
+      }
+    }
+  }
+
 }
