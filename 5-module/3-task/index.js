8993a63999a3662335eac7d10087147f1e9cb159
Dmitriy <d.a.lukashenko@ukr.net>
2021-11-07 18:05:09 +0200

    5-module 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
-function initCarousel() {
-  // ваш код...
+
+function initCarousel(event) {
+  let carouselInner = document.querySelector('.carousel__inner');
+  let slideWidth = carouselInner.offsetWidth;
+ 
+  let slidesCount = 0;
+  
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+
+  carouselArrowLeft.style.display = 'none';
+
+  carouselArrowLeft.addEventListener('click', () => {
+    slidesCount--;
+    carouselArrowRight.style.display = '';
+    if (slidesCount === 0) {
+      carouselArrowLeft.style.display = 'none';
+    }
+    if (slidesCount < 0) {
+      slidesCount = 0;
+    }
+    carouselInner.style.transform = `translateX(-${slideWidth * slidesCount}px)`;
+  });
+
+  carouselArrowRight.addEventListener('click', () => {
+    slidesCount++;
+    carouselArrowLeft.style.display = '';
+    if (slidesCount > 2) {
+      carouselArrowRight.style.display = 'none';
+    }
+    if (slidesCount > 3) {
+      slidesCount = 3;      
+    }
+    carouselInner.style.transform = `translateX(-${slideWidth * slidesCount}px)`;
+  });
+
 }
