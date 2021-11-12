e39d2680afee2dc8c6693bf4e926a50f5272cba7
Dmitriy <d.a.lukashenko@ukr.net>
2021-11-12 16:12:10 +0200

    6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,49 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+
 export default class UserTable {
-  constructor(rows) {
+
+  constructor(rows) {  
+    this.rows = rows;
+    this.trElem = ``;
+    for (let row of this.rows) {  
+      this.trElem += `<tr>
+      <td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><button>X</button></td>
+      </tr>`
+}   
+    this.render();
+
+    let buttons = this.elem.querySelectorAll('button');
+    for (let button of buttons) {
+      button.addEventListener('click', this.onClick);
+    }    
   }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = 
+    `<table>
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        ${this.trElem}
+    </tbody>
+  </table>
+`}
+
+  onClick = (event) => {
+    let trTarget = event.target.closest('tr');
+    trTarget.remove();
+
 }
+}
+
