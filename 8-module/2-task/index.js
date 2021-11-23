d0cdbd5c46f85eb80a24e69e9ecf751da9c56894
Dmitriy <d.a.lukashenko@ukr.net>
2021-11-23 10:59:53 +0200

    module-8 task-2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,68 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.filterAll = {};
+    this.cardRender = '';
+
+    this.products.forEach((product) => { 
+      this.cardRen(product);
+    });
+    
+
+    this.render();
+          
+  }  
+  
+  cardRen(product) {    
+      this.card = new ProductCard(product);
+      this.cardRender += this.card.elem.innerHTML;    
+  }
+
+  render() {
+    this.elem = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+        ${this.cardRender}
+      </div>
+    </div>
+    `);
+  } 
+  updateFilter(filters) {
+    
+    this.filtersAll = Object.assign(this.filters, filters);
+    this.elem.remove();
+    this.cardRender = '';
+    let container = document.getElementById('container');
+    
+
+    this.products.forEach((product) => {
+    
+      //noNuts filter
+      if (this.filtersAll.noNuts && !product.nuts) { 
+        this.cardRen(product);                 
+      }        
+      
+      //vegeterianOnly filter
+      else if (this.filtersAll.vegeterianOnly && product.vegeterian) { 
+        this.cardRen(product);                          
+      }       
+      
+      //maxSpiciness filter
+      else if (this.filtersAll.maxSpiciness && product.spiciness <= this.filtersAll.maxSpiciness) {
+        this.cardRen(product);  
+      }    
+
+      //category filter
+      else if (this.filtersAll.category && (this.filtersAll.category == product.category)) {  
+        this.cardRen(product);     
+      } 
+            
+      //without filters     
+      else if (!this.filtersAll.noNuts && !this.filtersAll.vegeterianOnly && !this.filtersAll.category && (!this.filtersAll.maxSpiciness || this.filtersAll.maxSpiciness === 4)) {        
+        this.cardRen(product);         
+      }
+    });
+    this.render(); 
+    container.append(this.elem);        
   }
 }
